f8169446702a703b377b4f1ba7f2ef674592d421
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-01-25 12:16:26 +0100

    better

--- nbinteract_fork/packages/nbinteract-core/src/emptyshim.js
+++
@@ -1,2 +0,0 @@
-// Empty JS file that replaces library modules
-export default {}
--- nbinteract_fork/packages/nbinteract/src/index.js
+++
@@ -1,9 +0,0 @@
-import NbInteract from 'nbinteract-core'
-
-const interact = new NbInteract({
-  baseUrl: 'https://mybinder.org',
-  spec: 'Calebs97/riemann_book/master',
-})
-window.interact = interact
-
-interact.prepare()
